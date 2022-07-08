596b4fe9504bb884ef916d3ef417e2ffb31a60ec
Igor` <webarts@yandex.ru>
2022-07-08 21:32:37 +0800

    Remove likePost from FeedNotifier, add statProvider init in _fetchFeedData

--- lib/provider/feed_provider/feed_provider.dart
+++ lib/provider/feed_provider/feed_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:fwitter/models/emoji/emoji_model.dart';
 import 'package:fwitter/models/fweet/fweet_model.dart';
+import 'package:fwitter/provider/stat_providers/stat_providers.dart';
 import 'package:fwitter/utils/network_base.dart';
 
 part '_feed_network.dart';
@@ -19,26 +19,8 @@ class FeedNotifier extends StateNotifier<AsyncValue<List<FweetModel>>> {
 
   Future<void> _fetchFeedData() async {
     state = await AsyncValue.guard(() async {
-      await Future.delayed(const Duration(milliseconds: 1200));
+      await ref.read(statProvider.future);
       return await _FeedNetwork().getFeeds();
     });
-  }
-
-  Future<void> likePost(int emojiId) async {
-    final s = state.whenOrNull(
-      data: (v) => v.map(
-        (e) => e.copyWith(
-          emojis: e.emojis
-              .map(
-                (e) => e.id == emojiId
-                    ? e
-                    : e.copyWith(count: e.count == 1 ? 0 : 1),
-              )
-              .toList(),
-        ),
-      ),
-    );
-
-    if (s != null) state = AsyncValue.data(s.toList());
   }
 }
